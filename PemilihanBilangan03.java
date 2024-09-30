af0be5627b6f464fb87aa845e9732be0d73164cc
Athaa10 <athamaulidia0904@gmail.com>
2024-09-30 11:04:23 +0700

    commit jobsheet 5

--- PemilihanBilangan03.java
+++ PemilihanBilangan03.java
@@ -3,17 +3,19 @@ public class PemilihanBilangan03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Masukkan sebuah angka: ");
-        int angka = sc.nextInt();
+        double angka;
+
+        String hasil;
 
         if (angka % 2 == 0)
         {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
+            hasil = "Angka " + angka + " termasuk bilangan genap";
         }
         else
         {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
+            hasil = "Angka " + angka + " termasuk bilangan ganjil";
         }
+        System.out.println(angka + " adalah " + hasil);
 
     }
 }
